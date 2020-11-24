15dc7c6208ffae7a92301e29c08f17e7e1ddcbe5
Yuki03280322 <72023246+Yuki03280322@users.noreply.github.com>
2020-11-24 11:11:34 +0900

    詳細ページリンク,ルーティング＋コントローラー設定

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -17,6 +17,10 @@ class ArtistsController < ApplicationController
     end
   end
 
+  def show
+    @artist = Artist.find(params[:id])
+  end
+
   private
 
   def artist_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :artists
   root to: "homes#index"
-  resources :artists, only: [:index, :edit, :update]
+  resources :artists, only: [:index, :edit, :update, :show]
   resources :arts, only: [:new, :create, :index, :destroy]
 end
