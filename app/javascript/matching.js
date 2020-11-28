33febe801e394cf8ac4eff068b2419409aeeeabf
Yuki03280322 <72023246+Yuki03280322@users.noreply.github.com>
2020-11-28 13:57:20 +0900

    使う画像準備

--- app/javascript/matching.js
+++ app/javascript/matching.js
@@ -1,6 +1,29 @@
+// const images = ["/assets/images/left-left.jpg", "app/assets/images/left-right.jpg", "app/assets/images/right-left.jpg", "app/assets/images/right-right.jpg"]
+
 if(document.URL.match(/matchings/)){
-  const matching = () => {
-    console.log("ok");
+  function changeImage(choice){
+    if(choice == left){
+      console.log("left");
+    }
+    if(choice == right){
+      console.log("right");
+    }
   };
-  window.addEventListener("load", matching);
-}
+  const leftImage = document.getElementsByClassName("left-image");
+  console.log(leftImage)
+  leftImage.addEventListener("click", changeImage(left));
+
+  const rightImage = document.getElementById("right");
+  console.log(rightImage)
+  // right.addEventListener("click", changeImage(right));
+
+
+  window.addEventListener("load", changeImage);
+};
+// document.getElementById("left").onclick = function(){
+//   changeImage("left");
+// };
+// document.getElementById("right").onclick = function(){
+//   changeImage("right");
+//   };
+
