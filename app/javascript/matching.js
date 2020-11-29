b4c2ea1666adf791222afcd241768883b0f7c412
Yuki03280322 <72023246+Yuki03280322@users.noreply.github.com>
2020-11-29 16:25:51 +0900

    画像id取得、画像クリックで発火OK

--- app/javascript/matching.js
+++ app/javascript/matching.js
@@ -1,32 +1,18 @@
-// const images = ["/assets/images/left-left.jpg", "app/assets/images/left-right.jpg", "app/assets/images/right-left.jpg", "app/assets/images/right-right.jpg"]
+const images = ["app/assets/images/left-left.jpg", "app/assets/images/left-right.jpg", "app/assets/images/right-left.jpg", "app/assets/images/right-right.jpg"]
 
 if(document.URL.match(/matchings/)){
-  function changeImage(choice){
-    if(choice == left){
-      console.log("left");
-    }
-    if(choice == right){
-      console.log("right");
-    }
+  function changeImage() {
+    const leftImage = document.getElementById("left-image");
+    leftImage.addEventListener("click", () => {
+      leftImage.src = images[0];
+      console.log(leftImage.src);
+    });
+    const rightImage = document.getElementById("right-image");
+    rightImage.addEventListener("click", () => {
+      rightImage.src = images[2];
+      console.log(rightImage.src);
+    });
   };
-  // const leftImage = document.getElementsByClassName("left-image");
-  // console.log(leftImage)
-  // leftImage.addEventListener("click", changeImage(left));
-
-  // const rightImage = document.getElementById("right-image");
-  // console.log(rightImage)
-  // right.addEventListener("click", changeImage(right));
-  document.getElementById("left-image").onclick = function(){
-    changeImage("left");
-  };
-  document.getElementById("right-image").onclick = function(){
-    changeImage("right");
-  };
+  window.addEventListener("load", changeImage)
 };
-// document.getElementById("left-image").onclick = function(){
-//   changeImage("left");
-// };
-// document.getElementById("right-image").onclick = function(){
-//   changeImage("right");
-// };
 
