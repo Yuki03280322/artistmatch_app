b98f1b73f44123bc08008442c4d1dbeadd58cebf
Yuki03280322 <72023246+Yuki03280322@users.noreply.github.com>
2020-11-30 11:56:44 +0900

    2回目のクリックで画像切替OK

--- app/javascript/matching.js
+++ app/javascript/matching.js
@@ -1,13 +1,19 @@
 const images = ["https://pbs.twimg.com/media/ETy62qVUEAYv314?format=jpg&name=large", "https://image.minne.com/minne/photo/640x640cq85p/da52206a2544c39c1c7bda3338082c1d11aabe49.jpeg/minne-bucket-pro.s3-ap-northeast-1.amazonaws.com/productimages/24563678/w1600xh1600/55f350470a6c413f7fceba64b3900c3a753d2470.jpeg?1478001745", "https://www.nigaoe-orange.com/images/gallery/yumi/201811sample04.jpg", "https://2gaoe.jp/cmswp/wp-content/uploads/jsystem_nigaoe/1550389866_img1.jpg"]
+let current = 0;
 
 if(document.URL.match(/matchings/)){
   function changeImage() {
     const leftImage = document.getElementById("left-image");
     leftImage.addEventListener("click", () => {
       leftImage.src = images[0];
-      rightImage.src = images[1]
-      console.log(leftImage.src);
-      console.log(rightImage.src);
+      rightImage.src = images[1];
+      current += 1;
+      if(current == 1){
+        leftImage.addEventListener("click", () => {
+          const leftImageTwo = document.getElementById("left-image");
+          leftImageTwo.src = images[3]
+        })
+      }
     });
     const rightImage = document.getElementById("right-image");
     rightImage.addEventListener("click", () => {
