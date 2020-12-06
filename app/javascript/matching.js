a71f0ccde17c553c9374fa554fa7c72b9d86e5f3
Yuki03280322 <72023246+Yuki03280322@users.noreply.github.com>
2020-12-06 18:29:09 +0900

    footer作成,使用画像修正

--- app/javascript/matching.js
+++ app/javascript/matching.js
@@ -1,4 +1,4 @@
-const images = ["https://pbs.twimg.com/media/ETy62qVUEAYv314?format=jpg&name=large", "https://image.minne.com/minne/photo/640x640cq85p/da52206a2544c39c1c7bda3338082c1d11aabe49.jpeg/minne-bucket-pro.s3-ap-northeast-1.amazonaws.com/productimages/24563678/w1600xh1600/55f350470a6c413f7fceba64b3900c3a753d2470.jpeg?1478001745", "https://www.nigaoe-orange.com/images/gallery/yumi/201811sample04.jpg", "https://2gaoe.jp/cmswp/wp-content/uploads/jsystem_nigaoe/1550389866_img1.jpg"];
+const images = ["https://pbs.twimg.com/media/EoRO67sVgAEdjUU?format=jpg&name=large", "https://base-ec2if.akamaized.net/w=640,a=0,q=90,u=1/images/item/origin/7584a5f38ec5332146af2a947baf4544.png", "https://pbs.twimg.com/media/EoSqRQDU4AAp1GT?format=jpg&name=900x900", "https://pbs.twimg.com/media/EoSqRQxVgAASumt?format=jpg&name=4096x4096"];
 let current = 0;
 
 if(document.URL.match(/matchings/)){
