55a8600f461b13d9375195145f0d79a17c981b7b
Yuki03280322 <72023246+Yuki03280322@users.noreply.github.com>
2021-01-10 18:31:37 +0900

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -33,32 +33,3 @@ end
 after_fork do |_server, _worker|
   defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
 end
-
-upstream app_server {
-  server unix:/var/www/artistmatch_app/shared/tmp/sockets/unicorn.sock;
-}
-
-server {
-  listen 80;
-  server_name 54.238.153.106;
-  client_max_body_size 2g;
-  root /var/www/artistmatch_app/current/public;
-
-  location ^~ /assets/ {
-    gzip_static on;
-    expires max;
-    add_header Cache-Control public;
-    root /var/www/artistmatch_app/current/public;
-  }
-
-  try_files $uri/index.html $uri @unicorn;
-
-  location @unicorn {
-    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-    proxy_set_header Host $http_host;
-    proxy_redirect off;
-    proxy_pass http://app_server;
-  }
-
-  error_page 500 502 503 504 /500.html;
-}
